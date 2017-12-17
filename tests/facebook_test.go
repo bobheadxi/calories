906c2587403c675e759fc164870bf0a693adce34
Seung Won Lee <seungwon.lee@alumni.ubc.ca>
2017-12-16 22:20:20 -0800

    Refactor facebook.api call

--- tests/facebook_test.go
+++ tests/facebook_test.go
@@ -13,13 +13,21 @@ import (
 	"github.com/bobheadxi/calories/facebook"
 )
 
-// TestNewFacebook : test API instantiation
-func TestNewFacebook(t *testing.T) {
+func apiSetUp() *facebook.API {
 	cfg := config.EnvConfig{
 		PageID: "123",
 		Token:  "321",
 	}
+	// var api facebook.API;
 	api := facebook.New(&cfg)
+	return api
+}
+
+// TestNewFacebook : test API instantiation
+func TestNewFacebook(t *testing.T) {
+
+	api := apiSetUp()
+
 	if api.MessageHandler != nil && api.PostbackHandler != nil {
 		t.Logf("Handler setup incorrect")
 	}
@@ -33,14 +41,10 @@ func TestNewFacebook(t *testing.T) {
 
 // TestSendTextMessage : Test sending a text message under different API
 // conditions (ie when Facebook is online and offline)
-func TestSendTextMessage(t *testing.T) {
-	cfg := config.EnvConfig{
-		PageID: "123",
-		Token:  "321",
-	}
-	api := facebook.New(&cfg)
-
+func TestSendTextMessageAvailable(t *testing.T) {
 	/* Test when server ok: Should make request with correct parameters */
+
+	api := apiSetUp()
 
 	// Set up fake Facebook server
 	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -50,27 +54,27 @@ func TestSendTextMessage(t *testing.T) {
 		// Check request method
 		if req.Method != "POST" {
 			t.Errorf("Request not POST")
-		}
+	}
 
-		// Check request body is as expected
-		read, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			t.Errorf("Bad request body: " + err.Error())
-		}
-		msg := &facebook.Message{}
-		err = json.Unmarshal(read, msg)
-		if err != nil {
-			t.Errorf("Bad message" + err.Error())
-		}
-		if msg.MessageType != "RESPONSE" {
-			t.Errorf("Wrong message type, expecting %s, got %s", "RESPONSE", msg.MessageType)
-		}
-		if msg.Package.Text != "Hello" {
-			t.Errorf("Wrong message text, expecting %s, got %s", "Hello", msg.Package.Text)
-		}
-		if msg.Recipient.ID != "789" {
-			t.Errorf("Wrong recipient, expecting %s, got %s", "789", msg.Recipient.ID)
-		}
+	// Check request body is as expected
+	read, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		t.Errorf("Bad request body: " + err.Error())
+	}
+	msg := &facebook.Message{}
+	err = json.Unmarshal(read, msg)
+	if err != nil {
+		t.Errorf("Bad message" + err.Error())
+	}
+	if msg.MessageType != "RESPONSE" {
+		t.Errorf("Wrong message type, expecting %s, got %s", "RESPONSE", msg.MessageType)
+	}
+	if msg.Package.Text != "Hello" {
+		t.Errorf("Wrong message text, expecting %s, got %s", "Hello", msg.Package.Text)
+	}
+	if msg.Recipient.ID != "789" {
+		t.Errorf("Wrong recipient, expecting %s, got %s", "789", msg.Recipient.ID)
+	}
 	}))
 	defer ts.Close()
 
@@ -80,11 +84,15 @@ func TestSendTextMessage(t *testing.T) {
 	if err != nil {
 		t.Errorf("Errored: " + err.Error())
 	}
+}
 
+func TestSendMessageUnavailable(t *testing.T) {
 	/* Test when server down: Should return an error */
 
+	api := apiSetUp()
+
 	// Set up a new fake server
-	ts = httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		// Return StatusServiceUnavailable when a request is made
 		rw.WriteHeader(http.StatusServiceUnavailable)
 	}))
@@ -92,7 +100,7 @@ func TestSendTextMessage(t *testing.T) {
 
 	// Set API to use fake server's URL and call SendTextMessage()
 	facebook.GraphAPI = ts.URL
-	err = api.SendTextMessage("789", "Hello")
+	err := api.SendTextMessage("789", "Hello")
 	// Should return error
 	if err == nil {
 		t.Errorf("No error returned")
