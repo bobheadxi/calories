b7bc7ea9cc572eacdde38954536c3805d21367c8
Robert Lin <robertlin1@gmail.com>
2017-12-16 22:02:31 -0800

    Reduce tick interval to test, test new ticker

--- bot/scheduler.go
+++ bot/scheduler.go
@@ -8,17 +8,22 @@ import (
 
 // scheduler : start and check every hour to send scheduled summaries to users
 func (b *Bot) scheduler() {
-	t := time.NewTicker(time.Hour * 1)
-	for {
-		b.sendSummary()
-		<-t.C
-	}
+	t := time.NewTicker(time.Second * 1)
+	go func() {
+		for _ = range t.C {
+			b.sendSummary()
+		}
+	}()
 }
 
 // sendSummary : appropriately send all users the scheduled summaries depending on their timezone
 func (b *Bot) sendSummary() {
+	summaryTime := 22 // 24-hour time for sending out summary
+
 	utcTime := time.Now().UTC()
-	timezone := 22 - utcTime.Hour()
+	timezone := summaryTime - utcTime.Hour() - 24
+	log.Print("Ticking... " + strconv.Itoa(utcTime.Hour()))
+	log.Print(timezone)
 	users, err := b.server.GetUsersInTimezone(timezone)
 	if err != nil {
 		log.Print("Get users failed: " + err.Error())
